cce09e4843611fceb30915f9b96b0ef340216bed
Ercan Duman <ercanduman30@gmail.com>
2017-10-10 15:26:06 +0300

    Database constants added, establishing connection finished

---
+++ src/main/java/ercanduman/oracle_db_connect/Constants.java
@@ -0,0 +1,20 @@
+package ercanduman.oracle_db_connect;
+
+/**
+ * Created on 10.10.2017.
+ */
+
+class Constants {
+    // Database connection variables
+    static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    static final String DB_DRIVER = "oracle.jdbc.OracleDriver";
+    static final String DB_USERNAME = "EDUMAN";
+    static final String DB_PASSWORD = "Erc787dmm";
+
+    // SQL queries
+    static final String SQL_SEARCH = "SELECT * FROM eduman.operand_config_test";
+    static final String SQL_INSERT = "INSERT INTO eduman.operand_config_test (operand_id, operand_name, operand_symbol) VALUES (?, ?, ?";
+    static final String SQL_UPDATE = "UPDATE eduman.operand_config_test SET operand_name   = ?, operand_symbol = ? WHERE operand_id = ?";
+    static final String SQL_DELETE = "DELETE FROM eduman.operand_config_test WHERE operand_id = ?";
+
+}
--- src/main/java/ercanduman/oracle_db_connect/MainActivity.java
+++ src/main/java/ercanduman/oracle_db_connect/MainActivity.java
@@ -1,10 +1,40 @@
 package ercanduman.oracle_db_connect;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import oracle.jdbc.driver.OracleDriver;
+
+
 public class MainActivity {
+    private static String prefix_error = "\nERROR> ";
+    private static String prefix_info = "\nINFO> ";
+
+    private static Connection connection;
+    private static ResultSet resultSet;
+
+    // For testing only change executionType value as S: Search, U:Update, D:Delete, I:Insert
+    private static char executionType = 'S';
+
     public static void main(String[] strings) {
-        System.out.println("New module!!");
+        establishConnection();
     }
 
+    private static boolean establishConnection() {
+        try {
+            Class.forName(ercanduman.oracle_db_connect.Constants.DB_DRIVER);
+            DriverManager.registerDriver(new OracleDriver());
+            connection = DriverManager.getConnection(Constants.DB_URL, Constants.DB_USERNAME, Constants.DB_PASSWORD);
+            System.out.println(prefix_info + "Database Connection is successful!");
+            return true;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println(prefix_error + "Database Connection is failed!");
+            return false;
+        }
+    }
 }
 
 
